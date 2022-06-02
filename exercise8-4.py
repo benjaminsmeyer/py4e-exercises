d44b36e9001579a5bc3015a33f4a955d934ec549
Ben Meyer <88737362+benjaminsmeyer@users.noreply.github.com>
2022-06-01 23:52:17 -0400

    third commit

---
+++ exercise8-4.py
@@ -0,0 +1,19 @@
+# Exercise 8.4
+# 8.4 Open the file romeo.txt and read it line by line. 
+# For each line, split the line into a list of words using the split() method. 
+# The program should build a list of words. 
+# For each word on each line check to see if the word is already in the list and if not append it to the list. 
+# When the program completes, sort and print the resulting words in alphabetical order.
+# You can download the sample data at http://www.py4e.com/code3/romeo.txt
+
+filename = input("Enter file name: ")
+filehandler = open(filename)
+contentList = list()
+
+for line in filehandler:
+    for l in line.split():
+        if l not in contentList:
+            contentList.append(l)
+
+contentList.sort()
+print(contentList)
