d63302d95ff89f20642ca612c6d3b014e33f8964
Benjamin Meyer <benjaminmeyer@Michaels-iMac.local>
2022-06-07 17:08:46 -0400

    eighth commit

---
+++ src/exercise12-3.py
@@ -0,0 +1,35 @@
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+amount = int(input("Enter count: "))
+position = int(input("Enter position: "))
+linksList = list()
+names = list()
+
+print('Retrieving:', url)
+
+for times in range(amount):
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    for tag in tags:
+        # print(tag.get('href', None))
+        linksList.append(tag.get('href', None))
+    names.append(linksList[position - 1])
+    print('Retrieving:', linksList[position - 1])
+    html = urllib.request.urlopen(linksList[position - 1], context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    linksList = list()
